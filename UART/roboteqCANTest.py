a24fd2af967f43ddb7f058545811da2fd3fde60b
jonathanperthel <55859124+jonathanperthel@users.noreply.github.com>
2022-02-25 15:42:27 -0600

    Updated can commands

--- UART/roboteqCANTest.py
+++ UART/roboteqCANTest.py
@@ -3,14 +3,16 @@ from PyRoboteq import roboteq_commands as cmds
 import time
  
 controller = RoboteqHandler(debug_mode=False, exit_on_interrupt=False)
-connected = controller.connect("/dev/ttyS0") # Insert your COM port (for windows) or /dev/tty{your_port} (Commonly /dev/ttyACM0) for linux.
+connected = controller.connect("/dev/ttyACM0") # Insert your COM port (for windows) or /dev/tty{your_port} (Commonly /dev/ttyACM0) for linux.
 count = 0
 
 while connected:
     temp = controller_volts = controller.read_value("?ICL", 2)
-    #voltage =  controller.send_raw_command("@02?V 1 0") #raw string to get temperature reading of roboteq node 2
-    if count%1000 == 0:
-        print(temp)
-        #print(voltage)
+    controller_volts1 = controller.read_value("@01?T", 0)
+    controller_volts2 = controller.read_value("@02?T", 0)     
+    if count%100 == 0:
+        #print(temp)
+        print(controller_volts1)
+        print(controller_volts2)
         #print("/n")
     count += 1
