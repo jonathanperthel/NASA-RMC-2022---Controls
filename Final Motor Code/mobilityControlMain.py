05be009cea3d20e0aa323a4bc25151b8c06c3b55
jonathanperthel <55859124+jonathanperthel@users.noreply.github.com>
2022-03-29 20:12:05 -0500

    Added mobility test files

---
+++ Final Motor Code/mobilityControlMain.py
@@ -0,0 +1,12 @@
+from mobilityMovementCommand import moveRover
+
+contr = moveRover.init_Roboteq()
+port, pack = moveRover.servoSetup()
+
+velocity = 0
+angle = 2048
+
+while True:
+    moveRover.wheelMotors(contr, velocity)
+    moveRover.moveServos(port, pack, angle, angle, angle, angle)
+
