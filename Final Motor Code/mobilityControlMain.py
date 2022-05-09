fac29c747dc4e1c499b9b4ef8ab8a6ffd9e7d8c3
jonathanperthel <55859124+jonathanperthel@users.noreply.github.com>
2022-05-09 13:28:36 -0500

    Added Roboclaw docs

--- Final Motor Code/mobilityControlMain.py
+++ Final Motor Code/mobilityControlMain.py
@@ -3,11 +3,11 @@ from mobilityMovementCommand import moveRover
 port, pack = moveRover.servoSetup()
 cont = moveRover.init_Roboteq()
 
-print(cont)
+#print(cont)
 velocity = 0
 angle = 1000
 
-#moveRover.moveServos(port, pack, angle, angle, angle, angle)
+moveRover.moveServos(port, pack, angle, angle, angle, angle)
 temp = moveRover.wheelMotors(cont, velocity)
 print(temp)
 '''
